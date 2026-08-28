aebcfb9c38ca93f149e0e81604a65016ed8d3fea
Farich Murobic <farichmurobiq11@gmail.com>
2026-08-28 21:28:05 +0700

    update: Latihan Read Strings with ArrayList

--- src/FundamentalJava/Array/Latihan/ReadStringsWithArrayList.java
+++ src/FundamentalJava/Array/Latihan/ReadStringsWithArrayList.java
@@ -1,12 +1,14 @@
 package FundamentalJava.Array.Latihan;
 
+// Latihan
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ReadStringsWithArrayList {
 
     public static void main(String[] args) {
-        
+
         ArrayList<String> array = getStrings();
         for (int i = 0; i < array.size(); i++)
             System.out.println(array.get(i));
