f3cb0a73c38fb1f630876709ab0280eaa5941138
Farich Murobic <farichmurobiq11@gmail.com>
2026-08-31 00:52:34 +0700

    update: Integer & Floating Point

--- src/FundamentalJava/TipeDataPrimitif/IntegerAndFloatingPoint.java
+++ src/FundamentalJava/TipeDataPrimitif/IntegerAndFloatingPoint.java
@@ -1,42 +1,49 @@
 package FundamentalJava.TipeDataPrimitif;
 
 public class IntegerAndFloatingPoint {
-    
+
     /**
      * Integers (Bilangan Bulat)
      * 
-     * Java memiliki empat tipe data integer, semuanya bertanda (signed) yang berarti bisa
-     * menyimpan nilai positif dan negatif. Java tidak memiliki tipe data integer tak bertanda (unsigned).
+     * Java memiliki empat tipe data integer, semuanya bertanda (signed) yang
+     * berarti bisa
+     * menyimpan nilai positif dan negatif. Java tidak memiliki tipe data integer
+     * tak bertanda (unsigned)..
      *
-     *  1, Byte      -128 to 127                                                 1 byte  (8 bits)
-     *  2, Short     -32768 to 32767                                             2 bytes (16 bits)
-     *  3, Int       -2,147,483,648 to 2,147,483,647                             4 bytes (32 bits)
-     *  4, Long      -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807     8 bytes (64 bits)
+     * 1, Byte -128 to 127 1 byte (8 bits)
+     * 2, Short -32768 to 32767 2 bytes (16 bits)
+     * 3, Int -2,147,483,648 to 2,147,483,647 4 bytes (32 bits)
+     * 4, Long -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807 8 bytes (64
+     * bits)
      *
-     *  Untuk membuat literal long, lo harus menambahkan L atau l di akhir angka, misalnya 9223372036854775807L
+     * Untuk membuat literal long, lo harus menambahkan L atau l di akhir angka,
+     * misalnya 9223372036854775807L
      *
-     *  Secara default, setiap nilai bilangan bulat adalah literal int (32-bit). Lo bisa menuliskannya dalam berbagai basis:
+     * Secara default, setiap nilai bilangan bulat adalah literal int (32-bit). Lo
+     * bisa menuliskannya dalam berbagai basis:
      *
-     *  1. Desimal (basis 10): Contohnya 10, 42.
-     *  2. Oktal (basis 8): Ditandai dengan awalan 0 (nol). Contoh: 010 (sama dengan 8 dalam desimal).
-     *     Angka 8 dan 9 tidak diizinkan dalam format ini.
-     *  3. Heksadesimal (basis 16): Ditandai dengan awalan 0x atau 0X. Contoh:
-     *     0x10 (sama dengan 16 dalam desimal). Angka 10-15 diwakili oleh huruf A-F.
-     *  4. Biner (basis 2): Ditambahkan sejak JDK 7. Ditandai dengan awalan 0b atau 0B.
-     *     Contoh: 0b1010 (sama dengan 10 dalam desimal).
+     * 1. Desimal (basis 10): Contohnya 10, 42.
+     * 2. Oktal (basis 8): Ditandai dengan awalan 0 (nol). Contoh: 010 (sama dengan
+     * 8 dalam desimal).
+     * Angka 8 dan 9 tidak diizinkan dalam format ini.
+     * 3. Heksadesimal (basis 16): Ditandai dengan awalan 0x atau 0X. Contoh:
+     * 0x10 (sama dengan 16 dalam desimal). Angka 10-15 diwakili oleh huruf A-F.
+     * 4. Biner (basis 2): Ditambahkan sejak JDK 7. Ditandai dengan awalan 0b atau
+     * 0B.
+     * Contoh: 0b1010 (sama dengan 10 dalam desimal).
      *
      * Floating-Point (Angka Desimal)
      * Tipe ini digunakan untuk angka dengan presisi pecahan.
      *
-     * 1, Float         1.4e–045 to 3.4e+038        4 bytes (32 bits)
-     * 2, Double        4.9e–324 to 1.8e+308        8 bytes (64 bits)
+     * 1, Float 1.4e–045 to 3.4e+038 4 bytes (32 bits)
+     * 2, Double 4.9e–324 to 1.8e+308 8 bytes (64 bits)
      *
-     *  Pemakaian floating di berbagai number:
-     *  1, Desimal - boleh
-     *  2, Oktal - tidak bisa untuk floating point
-     *  3, Heksadesimal - boleh, tapi hanya dengan p notation (scienfitic hex)
-     *     formatnya: 0x bilangan heksa p (eksponen berbasis 2)
-     *  4, Biner tidak bisa untuk floatijng point
+     * Pemakaian floating di berbagai number:
+     * 1, Desimal - boleh
+     * 2, Oktal - tidak bisa untuk floating point
+     * 3, Heksadesimal - boleh, tapi hanya dengan p notation (scienfitic hex)
+     * formatnya: 0x bilangan heksa p (eksponen berbasis 2)
+     * 4, Biner tidak bisa untuk floatijng point
      */
 
     public static void main(String[] args) {
@@ -55,13 +62,15 @@ public class IntegerAndFloatingPoint {
         short iniShortPlus = 32767;
         System.out.println("Ini batas short plus: " + iniShortPlus);
 
-        // int, Tipe integer yang paling sering digunakan. Sangat ideal untuk penghitung loop.
+        // int, Tipe integer yang paling sering digunakan. Sangat ideal untuk penghitung
+        // loop.
         int iniIntMinus = -2147483648;
         System.out.println("Ini int batas minus: " + iniIntMinus);
         int iniIntPlus = 2147483647;
         System.out.println("Ini int batas plus: " + iniIntPlus);
 
-        // long, Digunakan saat int tidak cukup besar, seperti pada perhitungan jarak cahaya.
+        // long, Digunakan saat int tidak cukup besar, seperti pada perhitungan jarak
+        // cahaya.
         long iniLongMinus = -9223372036854775808L; // Note diakhiri huruf L untuk setiap Long
         System.out.println("Ini batas long minus: " + iniLongMinus);
         long iniLongPlus = 9223372036854775807L; // Note diakhiri huruf L untuk setiap Long
@@ -80,7 +89,8 @@ public class IntegerAndFloatingPoint {
 
         System.out.println("\n--- Literal integer bilangan oktal basis 8 ---");
         /**
-         * Oktal (basis 8): Ditandai dengan awalan 0 (nol). Contoh: 010 (sama dengan 8 dalam desimal).
+         * Oktal (basis 8): Ditandai dengan awalan 0 (nol). Contoh: 010 (sama dengan 8
+         * dalam desimal).
          * Angka 8 dan 9 tidak diizinkan dalam format ini.
          * digit 0-7
          * jadi kalau sebuah angka diawali nol (0) dan hanya pakai digit 0-7
@@ -115,7 +125,8 @@ public class IntegerAndFloatingPoint {
         System.out.println(heksaDesimal3);
         int heksaDesimalMinus = -0xA; // -10
         System.out.println(heksaDesimalMinus);
-        // int heksaDesimal4 = 0xg; // akan error, karena g bukan digit valid heksadesimal (maksimum F)
+        // int heksaDesimal4 = 0xg; // akan error, karena g bukan digit valid
+        // heksadesimal (maksimum F)
 
         // Contoh lain heksadesimal
         int intHexa = 0xA132B;
@@ -129,7 +140,8 @@ public class IntegerAndFloatingPoint {
 
         System.out.println("\n--- Literal biner basis 2 ---");
         /**
-         * Biner (basis 2): Ditambahkan sejak JDK 7. Ditandai dengan awalan 0b atau 0B. Contoh:
+         * Biner (basis 2): Ditambahkan sejak JDK 7. Ditandai dengan awalan 0b atau 0B.
+         * Contoh:
          * 0b1010 (sama dengan 10 dalam desimal).
          */
 
@@ -146,7 +158,8 @@ public class IntegerAndFloatingPoint {
          * 0b1111 dihitung sebagai
          * (1 x 2^3) + (1 x 2^2) + (1 x 2^1) + (1 x 2^0) = 8 + 4 + 2 + 1 = 15
          * 0b100000
-         * (1 x 2^5) + (0 x 2^4) + (0 x 2^3) + (0 x 2^2) + (0 x 2^1) + (0 x 2^0) = 32 + 0 + 0 + 0 + 0 = 32
+         * (1 x 2^5) + (0 x 2^4) + (0 x 2^3) + (0 x 2^2) + (0 x 2^1) + (0 x 2^0) = 32 +
+         * 0 + 0 + 0 + 0 = 32
          */
 
         // contoh lanjutan
@@ -161,7 +174,8 @@ public class IntegerAndFloatingPoint {
 
         /**
          * Note : Seperti konversi dari Hexa dan Binary ke tipe data yang diatas,
-         * mungkin byte dan short bisa kalau nilai dari hexa dan biner mencakup kapasitasnya.
+         * mungkin byte dan short bisa kalau nilai dari hexa dan biner mencakup
+         * kapasitasnya.
          */
 
         System.out.println("\n--- Kode Underscore (_) untuk pemisah di input bukan output ---");
@@ -185,15 +199,19 @@ public class IntegerAndFloatingPoint {
 
         System.out.println("\n--- Tipe data Floating Point ---");
 
-        // float, Presisi tunggal, lebih kecil. Cocok untuk data yang tidak memerlukan akurasi tinggi.
+        // float, Presisi tunggal, lebih kecil. Cocok untuk data yang tidak memerlukan
+        // akurasi tinggi.
         float iniFloat = 10.10F; // Note diakhiri huruf F untuk setiap Float
         System.out.println("Ini float: " + iniFloat);
-        System.out.println("Kapasitas Float: float Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits");
+        System.out.println(
+                "Kapasitas Float: float Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits");
 
-        // double, Presisi ganda, lebih akurat. Ini adalah pilihan default untuk perhitungan matematis kompleks.
+        // double, Presisi ganda, lebih akurat. Ini adalah pilihan default untuk
+        // perhitungan matematis kompleks.
         double iniDouble = 10.10;
         System.out.println("Ini double: " + iniDouble);
-        System.out.println("Kapasitas Double: Stores fractional numbers. Sufficient for storing 15 to 16 decimal digits");
+        System.out
+                .println("Kapasitas Double: Stores fractional numbers. Sufficient for storing 15 to 16 decimal digits");
 
         System.out.println("\n--- Floating Point desimal ---");
         double num = 9_423_497_862.0;
