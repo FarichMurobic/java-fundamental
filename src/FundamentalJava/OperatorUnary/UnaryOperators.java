2887211353962ac7228665cb99d474c42e475507
Farich Murobic <farichmurobiq11@gmail.com>
2026-08-31 00:39:35 +0700

    update: Unary Operator

--- src/FundamentalJava/OperatorUnary/UnaryOperators.java
+++ src/FundamentalJava/OperatorUnary/UnaryOperators.java
@@ -4,18 +4,18 @@ public class UnaryOperators {
     public static void main(String[] args) {
 
         /**
-         * Unary operator adalah operator yang bekerja pada satu operand saja.
+         * Unary operator adalah operator yang bekerja pada satu operand saja..
          * 
          * Contoh unary operator di java:
          *
-         * Operator	    Fungsi
-         * ++	        increment
-         * --	        decrement
-         * !	        logical NOT
-         * ~	        bitwise NOT
-         * +	        unary plus
-         * -	        unary minus
-         * (type)	    type casting
+         * Operator Fungsi
+         * ++ increment
+         * -- decrement
+         * ! logical NOT
+         * ~ bitwise NOT
+         * + unary plus
+         * - unary minus
+         * (type) type casting
          *
          * Associavity unary operator
          * Unary operator memiliki right to left associavity
