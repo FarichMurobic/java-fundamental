2737a19f3fadd16d6cb4b6071676ecd7c34ab482
Farich Murobic <farichmurobiq11@gmail.com>
2026-08-31 00:04:25 +0700

    update: Literals

--- src/FundamentalJava/LexicalIssues/Literals.java
+++ src/FundamentalJava/LexicalIssues/Literals.java
@@ -4,11 +4,12 @@ public class Literals {
 
     /**
      * Literals (Nilai Konstan)
-     * Literals adalah representasi nilai konstan dalam kode. Mereka digunakan untuk menetapkan nilai secara langsung.
-     * •	100 adalah literal integer.
-     * •	98.6 adalah literal floating-point (angka desimal).
-     * •	'X' adalah literal karakter.
-     * •	"Ini adalah tes" adalah literal string.
+     * Literals adalah representasi nilai konstan dalam kode. Mereka digunakan untuk
+     * menetapkan nilai secara langsung.
+     * • 100 adalah literal integer.
+     * • 98.6 adalah literal floating-point (angka desimal).
+     * • 'X' adalah literal karakter.
+     * • "Ini adalah tes" adalah literal string..
      */
 
 }
