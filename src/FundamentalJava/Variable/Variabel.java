6f14ddb6918c417ea5cc344d21c81c4c513a55db
Farich Murobic <farichmurobiq11@gmail.com>
2026-08-31 00:58:43 +0700

    update: Variable

--- src/FundamentalJava/Variable/Variabel.java
+++ src/FundamentalJava/Variable/Variabel.java
@@ -3,16 +3,19 @@ package FundamentalJava.Variable;
 /**
  * Variabel
  * 
- * Variabel adalah unit dasar penyimpanan dalam program Java. Variabel didefinisikan oleh:
- * -	Tipe: Jenis data yang bisa disimpan.
- * -	Identifier: Nama variabel.
- * -	Inisialisasi (opsional): Nilai awal yang ditetapkan.
- * -	Lingkup (Scope): Bagian program di mana variabel terlihat.
- * -	Masa Hidup (Lifetime): Periode di mana variabel itu ada.
+ * Variabel adalah unit dasar penyimpanan dalam program Java. Variabel
+ * didefinisikan oleh:
+ * - Tipe: Jenis data yang bisa disimpan.
+ * - Identifier: Nama variabel.
+ * - Inisialisasi (opsional): Nilai awal yang ditetapkan.
+ * - Lingkup (Scope): Bagian program di mana variabel terlihat.
+ * - Masa Hidup (Lifetime): Periode di mana variabel itu ada.
  *
- * Deklarasi dan Inisialisasi Dinamis
- * Semua variabel harus dideklarasikan sebelum digunakan. Lo bisa mendeklarasikan beberapa
- * variabel sekaligus dengan tipe yang sama menggunakan koma. Inisialisasi bisa dilakukan secara
+ * Deklarasi dan Inisialisasi Dinamis.
+ * Semua variabel harus dideklarasikan sebelum digunakan. Lo bisa
+ * mendeklarasikan beberapa
+ * variabel sekaligus dengan tipe yang sama menggunakan koma. Inisialisasi bisa
+ * dilakukan secara
  * dinamis, menggunakan ekspresi yang valid pada saat deklarasi.
  */
 
@@ -23,7 +26,8 @@ public class Variabel {
         /**
          * VARIABEL ADALAH TEMPAT UNTUK MENYIMPAN DATA
          *
-         * Di Java, variabel harus dideklarasikan dengan tipe data sebelum digunakan. Format umumnya:
+         * Di Java, variabel harus dideklarasikan dengan tipe data sebelum digunakan.
+         * Format umumnya:
          * TipeData namaVariabel = nilai;
          *
          * Aturan umum untuk penamaan variabel adalah :
@@ -31,8 +35,10 @@ public class Variabel {
          * -Nama harus dimulai dengan huruf tidak boleh angka
          * -Nama harus dimulai dengan huruf kecil, dan tidak boleh mengandung spasi
          * -Nama juga bisa dimulai dengan $ dan _
-         * -Nama peka huruf besar/kecil ("myVar" dan "myvar" adalah variabel yang berbeda)
-         * -Kata-kata yang dicadangkan (seperti kata kunci Java, seperti int atau boolean) tidak dapat digunakan sebagai nama variabel
+         * -Nama peka huruf besar/kecil ("myVar" dan "myvar" adalah variabel yang
+         * berbeda)
+         * -Kata-kata yang dicadangkan (seperti kata kunci Java, seperti int atau
+         * boolean) tidak dapat digunakan sebagai nama variabel
          */
 
         byte variabelByte;
@@ -50,27 +56,35 @@ public class Variabel {
         // double3 is dynamically initialized
         double double3 = Math.sqrt(double1 * double1 + double2 * double2);
         System.out.println("Hypotenuse is " + double3);
-        // Di sini, nilai double3 dihitung menggunakan hasil dari ekspresi Math.sqrt(...), bukan nilai konstan.
+        // Di sini, nilai double3 dihitung menggunakan hasil dari ekspresi
+        // Math.sqrt(...), bukan nilai konstan.
 
         System.out.println();
 
         /**
          * Lingkup dan Masa Hidup Variabel
          * 
-         * Dalam Java, lingkup (scope) didefinisikan oleh blok kode, yaitu area di antara kurung kurawal
+         * Dalam Java, lingkup (scope) didefinisikan oleh blok kode, yaitu area di
+         * antara kurung kurawal
          * { dan }. Lingkup menentukan visibilitas dan masa hidup variabel.
          *
          * Aturan Penting tentang Lingkup:
-         * 1.	Variabel Lokal: Variabel yang dideklarasikan di dalam sebuah blok hanya terlihat
-         *      dan dapat diakses di dalam blok itu saja.
-         * 2.	Lingkup Bersarang: Lingkup bisa bersarang. Variabel yang dideklarasikan di lingkup luar (outer scope)
-         *      dapat diakses oleh kode di lingkup dalam (inner scope), tetapi tidak sebaliknya.
-         * 3.	Masa Hidup: Variabel diciptakan saat lingkupnya dimasuki dan dihancurkan saat lingkupnya ditinggalkan.
-         *      Ini berarti nilai variabel tidak akan bertahan jika lingkupnya berakhir.
-         * 4.	Inisialisasi Ulang: Jika sebuah variabel diinisialisasi di dalam sebuah blok,
-         *      ia akan diinisialisasi ulang setiap kali blok itu dimasuki.
-         * 5.	Nama Variabel: Lo tidak bisa mendeklarasikan variabel dengan nama yang sama di lingkup dalam
-         *      jika nama tersebut sudah digunakan di lingkup luar.
+         * 1. Variabel Lokal: Variabel yang dideklarasikan di dalam sebuah blok hanya
+         * terlihat
+         * dan dapat diakses di dalam blok itu saja.
+         * 2. Lingkup Bersarang: Lingkup bisa bersarang. Variabel yang dideklarasikan di
+         * lingkup luar (outer scope)
+         * dapat diakses oleh kode di lingkup dalam (inner scope), tetapi tidak
+         * sebaliknya.
+         * 3. Masa Hidup: Variabel diciptakan saat lingkupnya dimasuki dan dihancurkan
+         * saat lingkupnya ditinggalkan.
+         * Ini berarti nilai variabel tidak akan bertahan jika lingkupnya berakhir.
+         * 4. Inisialisasi Ulang: Jika sebuah variabel diinisialisasi di dalam sebuah
+         * blok,
+         * ia akan diinisialisasi ulang setiap kali blok itu dimasuki.
+         * 5. Nama Variabel: Lo tidak bisa mendeklarasikan variabel dengan nama yang
+         * sama di lingkup dalam
+         * jika nama tersebut sudah digunakan di lingkup luar.
          */
 
         // Program ini menunjukkan masa hidup variabel y yang pendek
@@ -82,8 +96,10 @@ public class Variabel {
             System.out.println("y is now: " + y); // y berubah
         } // y dihancurkan disini
 
-        // Setiap kali for loop berulang, variabel y baru dibuat, diinisialisasi ke -1, dan setelah perulangan
-        // selesai, y dihancurkan, sehingga nilainya tidak bertahan untuk iterasi berikutnya
+        // Setiap kali for loop berulang, variabel y baru dibuat, diinisialisasi ke -1,
+        // dan setelah perulangan
+        // selesai, y dihancurkan, sehingga nilainya tidak bertahan untuk iterasi
+        // berikutnya
 
         int scope1;
         scope1 = 10;
