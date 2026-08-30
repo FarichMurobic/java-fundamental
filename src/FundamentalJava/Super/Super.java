08a84cc6bd14f3c4da16167a53eefcc58d2a77ab
Farich Murobic <farichmurobiq11@gmail.com>
2026-08-31 00:51:47 +0700

    update: Super

--- src/FundamentalJava/Super/Super.java
+++ src/FundamentalJava/Super/Super.java
@@ -6,7 +6,7 @@ package FundamentalJava.Super;
  * ------------------------------------------------------------------------
  * 
  * Java menyediakan keyword super untuk:
- * Mengakses superclass dari subclass.
+ * Mengakses superclass dari subclass...
  * 
  * Ada 2 fungsi utama super:
  * 1. Memanggil constructor superclass → super()
@@ -18,16 +18,16 @@ package FundamentalJava.Super;
  * 
  * Pada contoh sebelumnya, BoxWeight dibuat dengan cara yang tidak efisien.
  * 
- *     class BoxWeight extends Box {
- *         double weight;
+ * class BoxWeight extends Box {
+ * double weight;
  * 
- *         BoxWeight(double w, double h, double d, double m) {
- *             width = w;      // Duplikasi!
- *             height = h;     // Duplikasi!
- *             depth = d;      // Duplikasi!
- *             weight = m;
- *         }
- *     }
+ * BoxWeight(double w, double h, double d, double m) {
+ * width = w; // Duplikasi!
+ * height = h; // Duplikasi!
+ * depth = d; // Duplikasi!
+ * weight = m;
+ * }
+ * }
  * 
  * Kenapa ini buruk?
  * - Duplikasi kode (width, height, depth diisi ulang)
@@ -41,14 +41,14 @@ package FundamentalJava.Super;
  * SOLUSI: super() UNTUK MEMANGGIL CONSTRUCTOR SUPERCLASS
  * ------------------------------------------------------------------------
  * 
- *     class BoxWeight extends Box {
- *         double weight;
+ * class BoxWeight extends Box {
+ * double weight;
  * 
- *         BoxWeight(double w, double h, double d, double m) {
- *             super(w, h, d);  // Panggil constructor Box
- *             weight = m;      // Hanya isi yang unik
- *         }
- *     }
+ * BoxWeight(double w, double h, double d, double m) {
+ * super(w, h, d); // Panggil constructor Box
+ * weight = m; // Hanya isi yang unik
+ * }
+ * }
  * 
  * ATURAN PALING PENTING:
  * super() HARUS menjadi baris PERTAMA di constructor.
@@ -58,7 +58,7 @@ package FundamentalJava.Super;
  * ------------------------------------------------------------------------
  * 
  * Contoh:
- *     BoxWeight mybox = new BoxWeight(10, 20, 15, 34.3);
+ * BoxWeight mybox = new BoxWeight(10, 20, 15, 34.3);
  * 
  * Step-by-step:
  * 
@@ -79,35 +79,36 @@ package FundamentalJava.Super;
  * SUPERCLASS TANPA DEFAULT CONSTRUCTOR
  * ------------------------------------------------------------------------
  * 
- * Jika superclass tidak memiliki default constructor (constructor tanpa parameter),
+ * Jika superclass tidak memiliki default constructor (constructor tanpa
+ * parameter),
  * maka subclass HARUS memanggil super() secara eksplisit.
  * 
- *     class Box {
- *         double width, height, depth;
+ * class Box {
+ * double width, height, depth;
  * 
- *         // Hanya ada constructor dengan parameter
- *         Box(double w, double h, double d) {
- *             width = w;
- *             height = h;
- *             depth = d;
- *         }
- *     }
+ * // Hanya ada constructor dengan parameter
+ * Box(double w, double h, double d) {
+ * width = w;
+ * height = h;
+ * depth = d;
+ * }
+ * }
  * 
- *     class BoxWeight extends Box {
- *         double weight;
+ * class BoxWeight extends Box {
+ * double weight;
  * 
- *         BoxWeight(double w, double h, double d, double m) {
- *             super(w, h, d);  // WAJIB! Karena Box tidak punya default constructor
- *             weight = m;
- *         }
- *     }
+ * BoxWeight(double w, double h, double d, double m) {
+ * super(w, h, d); // WAJIB! Karena Box tidak punya default constructor
+ * weight = m;
+ * }
+ * }
  * 
  * ------------------------------------------------------------------------
  * BENTUK KEDUA: super.member UNTUK AKSES MEMBER SUPERCLASS
  * ------------------------------------------------------------------------
  * 
  * Bentuk kedua dari super digunakan seperti ini:
- *     super.member
+ * super.member
  * 
  * Di mana member bisa berupa:
  * - Variabel
@@ -123,55 +124,55 @@ package FundamentalJava.Super;
  * CONTOH NAME HIDING
  * ------------------------------------------------------------------------
  * 
- *     class A {
- *         int i;
- *     }
+ * class A {
+ * int i;
+ * }
  * 
- *     class B extends A {
- *         int i;  // Menutupi (hide) i dari A
+ * class B extends A {
+ * int i; // Menutupi (hide) i dari A
  * 
- *         B(int a, int b) {
- *             super.i = a;  // Isi i milik A
- *             i = b;        // Isi i milik B
- *         }
+ * B(int a, int b) {
+ * super.i = a; // Isi i milik A
+ * i = b; // Isi i milik B
+ * }
  * 
- *         void show() {
- *             System.out.println("i in superclass: " + super.i);
- *             System.out.println("i in subclass: " + i);
- *         }
- *     }
+ * void show() {
+ * System.out.println("i in superclass: " + super.i);
+ * System.out.println("i in subclass: " + i);
+ * }
+ * }
  * 
- *     public class UseSuper {
- *         public static void main(String[] args) {
- *             B subOb = new B(1, 2);
- *             subOb.show();
- *         }
- *     }
+ * public class UseSuper {
+ * public static void main(String[] args) {
+ * B subOb = new B(1, 2);
+ * subOb.show();
+ * }
+ * }
  * 
  * ------------------------------------------------------------------------
  * ALUR PROGRAM
  * ------------------------------------------------------------------------
  * 
  * 1. Buat object:
- *     B subOb = new B(1, 2);
+ * B subOb = new B(1, 2);
  * 
  * 2. Masuk constructor B:
- *     super.i = 1;  // i di A = 1
- *     i = 2;        // i di B = 2
+ * super.i = 1; // i di A = 1
+ * i = 2; // i di B = 2
  * 
  * 3. Output:
- *     i in superclass: 1
- *     i in subclass: 2
+ * i in superclass: 1
+ * i in subclass: 2
  * 
  * ------------------------------------------------------------------------
  * PERBEDAAN super() vs super.member
  * ------------------------------------------------------------------------
  * 
- * Bentuk           | Fungsi
+ * Bentuk | Fungsi
  * -----------------|------------------------------------------
- * super()          | Memanggil constructor superclass
- * super.variabel   | Mengakses variabel superclass
- * super.method()   | Memanggil method superclass
+ * super() | Memanggil constructor superclass
+ * super.variabel | Mengakses variabel superclass
+ * super.method() | Memanggil method superclass
  * 
  * ------------------------------------------------------------------------
  * INTI PALING PENTING
@@ -279,7 +280,7 @@ class BoxWeight extends Box {
     /**
      * Ini yang keren:
      * BoxWeight(BoxWeight ob) {
-     *     super(ob);
+     * super(ob);
      * }
      *
      * ob itu:
