f83de542eca1129062f389f54d2984c436edc3a7
Farich Murobic <farichmurobiq11@gmail.com>
2026-08-31 00:41:38 +0700

    update: CaraMembuatObjectTanpaImport

--- src/FundamentalJava/Package/Package2/CaraMembuatObjectTanpaImport.java
+++ src/FundamentalJava/Package/Package2/CaraMembuatObjectTanpaImport.java
@@ -4,7 +4,7 @@ package FundamentalJava.Package.Package2;
 public class CaraMembuatObjectTanpaImport {
     public static void main(String[] args) {
 
-        // membuat object Class Protection, berbeda Package, TANPA IMPORT
+        // membuat object Class Protection, berbeda Package, TANPA .
         // Package.Package1.Protection protection = new Package.Package1.Protection();
 
         // protection.methodePublic();
