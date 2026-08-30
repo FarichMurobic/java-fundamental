52aa184c8e1ab60ff6941307a98ce271db968ecb
Farich Murobic <farichmurobiq11@gmail.com>
2026-08-31 00:41:16 +0700

    update: Diffrent Package

--- src/FundamentalJava/Package/Package2/BedaPackage.java
+++ src/FundamentalJava/Package/Package2/BedaPackage.java
@@ -2,7 +2,7 @@ package FundamentalJava.Package.Package2;
 
 import FundamentalJava.Package.Package1.Protection;
 
-// class biasa beda package bukan subclass, mencoba akses Protection
+// class biasa beda package bukan subclass, mencoba akses Protection.
 public class BedaPackage {
     public BedaPackage() {
         Protection p = new Protection();
@@ -14,20 +14,20 @@ public class BedaPackage {
         // System.out.println(p.protectedVariabel); // protected, error
         System.out.println(p.publicVariabel); // public bisa
 
-       // mencoba akses methode
-       // p.methodeDefault(); // error
-       // p.methodePrivate(); // error
-       // p.methodeProtected(); // error
-       p.methodePublic(); // error
+        // mencoba akses methode
+        // p.methodeDefault(); // error
+        // p.methodePrivate(); // error
+        // p.methodeProtected(); // error
+        p.methodePublic(); // error
 
-       /**
-        * beda package bukan subclass
-        *
-        * default tidak bisa
-        * private tidak bisa
-        * protected tidak bisa
-        * public bisa
-        */
+        /**
+         * beda package bukan subclass
+         *
+         * default tidak bisa
+         * private tidak bisa
+         * protected tidak bisa
+         * public bisa
+         */
 
-   }
+    }
 }
