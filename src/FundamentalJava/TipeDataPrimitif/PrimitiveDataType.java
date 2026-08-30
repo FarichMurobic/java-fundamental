43d4ddf8950ef3f982441f10074a39a0f64416f9
Farich Murobic <farichmurobiq11@gmail.com>
2026-08-31 00:52:49 +0700

    update: Primitive Data Type

--- src/FundamentalJava/TipeDataPrimitif/PrimitiveDataType.java
+++ src/FundamentalJava/TipeDataPrimitif/PrimitiveDataType.java
@@ -3,10 +3,11 @@ package FundamentalJava.TipeDataPrimitif;
 public class PrimitiveDataType {
 
     /**
-     * Java adalah bahasa yang strongly typed.
+     * Java adalah bahasa yang strongly typed..
      * 
      * Ini berarti setiap variabel dan ekspresi harus memiliki tipe data yang jelas,
-     * dan semua penugasan (assignment) harus kompatibel. Kompilator Java akan memeriksa semua ini,
+     * dan semua penugasan (assignment) harus kompatibel. Kompilator Java akan
+     * memeriksa semua ini,
      * dan jika ada ketidaksesuaian tipe, program tidak akan berhasil dikompilasi.
      * Ini adalah salah satu alasan kenapa Java dikenal aman dan kuat.
      */
@@ -15,17 +16,19 @@ public class PrimitiveDataType {
      * Tipe Data Primitif (Primitive Types)
      * 
      * Java memiliki delapan tipe data primitif, yang dibagi menjadi empat kelompok:
-     * 1.	Integer (Bilangan Bulat): byte, short, int, long.
-     * 2.	Floating-Point (Angka Desimal): float, double.
-     * 3.	Characters (Karakter): char.
-     * 4.	Boolean (Logika): boolean.
+     * 1. Integer (Bilangan Bulat): byte, short, int, long.
+     * 2. Floating-Point (Angka Desimal): float, double.
+     * 3. Characters (Karakter): char.
+     * 4. Boolean (Logika): boolean.
      *
-     * Tipe primitif ini bukan objek, melainkan nilai tunggal. Ini dilakukan untuk efisiensi performa.
+     * Tipe primitif ini bukan objek, melainkan nilai tunggal. Ini dilakukan untuk
+     * efisiensi performa.
      */
 
     public static void main(String[] args) {
 
-        // Primitive Types, Tipe data merepresentasikan sesuai proporsinya masing-masing!
+        // Primitive Types, Tipe data merepresentasikan sesuai proporsinya
+        // masing-masing!
         byte thisByte = 10;
         System.out.println(thisByte);
         short thisShort = 100;
