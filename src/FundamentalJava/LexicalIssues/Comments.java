be881d375ae075de2d7e0d276f68c067222c9339
Farich Murobic <farichmurobiq11@gmail.com>
2026-08-31 00:03:28 +0700

    update: Comments

--- src/FundamentalJava/LexicalIssues/Comments.java
+++ src/FundamentalJava/LexicalIssues/Comments.java
@@ -2,24 +2,27 @@ package FundamentalJava.LexicalIssues;
 
 public class Comments {
 
-     /**
-     * Comments (Komentar)
-     * Ada tiga jenis komentar dalam Java:
-     * -Komentar satu baris. Semua yang ada setelah // hingga akhir baris akan diabaikan.
-     * -Komentar multi-baris. Semua yang ada di antara diabaikan, bisa mencakup beberapa baris.
-     * -Komentar dokumentasi. Ini adalah tipe khusus yang digunakan untuk menghasilkan file dokumentasi HTML otomatis menggunakan alat javadoc.
-     *
-     * Contoh:
-     */
-
-    // Single-line
-
-    /*
-       Multiline
+   /**
+    * Comments (Komentar)
+    * Ada tiga jenis komentar dalam Java:
+    * -Komentar satu baris. Semua yang ada setelah // hingga akhir baris akan
+    * diabaikan.
+    * -Komentar multi-baris. Semua yang ada di antara diabaikan, bisa mencakup
+    * beberapa baris.
+    * -Komentar dokumentasi. Ini adalah tipe khusus yang digunakan untuk
+    * menghasilkan file dokumentasi HTML otomatis menggunakan alat javadoc..
+    *
+    * Contoh:
     */
 
-    /**
-     * Documentation Comment
-     */
+   // Single-line
+
+   /*
+    * Multiline
+    */
+
+   /**
+    * Documentation Comment
+    */
 
 }
