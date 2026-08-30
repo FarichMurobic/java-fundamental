6ce29a758963d23d7ccc3f91f4aca59278a18042
Farich Murobic <farichmurobiq11@gmail.com>
2026-08-31 00:44:20 +0700

    update: MaxTest2

--- src/FundamentalJava/Scanner/Latihan/MaxTest2.java
+++ src/FundamentalJava/Scanner/Latihan/MaxTest2.java
@@ -1,11 +1,13 @@
 package FundamentalJava.Scanner.Latihan;
 
+// LATIHAN
+
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class MaxTest2 {
     public static void main(String[] args) {
-        
+
         Scanner in = new Scanner(System.in);
 
         System.out.println("Enter 2 ints on one line: ");
