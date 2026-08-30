89ee2930e15b2bd1a598190ef819886b5beaaa98
Farich Murobic <farichmurobiq11@gmail.com>
2026-08-31 00:04:49 +0700

    update: WhiteSpace

--- src/FundamentalJava/LexicalIssues/WhiteSpace.java
+++ src/FundamentalJava/LexicalIssues/WhiteSpace.java
@@ -6,9 +6,10 @@ public class WhiteSpace {
      * Elemen-Elemen Dasar Java
      *
      * Whitespace adalah karakter spasi, tab, dan baris baru (newline).
-     * Java adalah bahasa yang free-form, artinya lo bisa menempatkan kode di mana saja,
+     * Java adalah bahasa yang free-form, artinya lo bisa menempatkan kode di mana
+     * saja,
      * asalkan ada spasi atau pemisah yang tepat di antara elemen-elemennya.
-     * Indentasi tidak wajib, tapi sangat disarankan untuk keterbacaan kode.
+     * Indentasi tidak wajib, tapi sangat disarankan untuk keterbacaan kode..
      */
 
 }
